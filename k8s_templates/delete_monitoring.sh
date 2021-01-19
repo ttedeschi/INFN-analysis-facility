16ee7698e5e5673a09d07e30c26e4c4aa6b1883a
Tommaso Tedeschi <tedesk95@gmail.com>
2021-01-19 16:57:58 +0100

    Update delete_monitoring.sh

--- k8s_templates/delete_monitoring.sh
+++ k8s_templates/delete_monitoring.sh
@@ -2,4 +2,4 @@ kubectl delete -f htcondor-exporter.yaml
 kubectl delete configmap prometheus-example-cm -n monitoring
 kubectl delete -f prometheus.yaml
 kubectl delete configmap prometheus-adapter-example-cm 
-kubectl delete -f prometheus_adapter.yaml 
+kubectl delete -f prometheus-adapter.yaml 
