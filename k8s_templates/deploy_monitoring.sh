5cbce3b68e7cf805e78c2b3884217375a79e7f6f
Tommaso Tedeschi <tedesk95@gmail.com>
2021-01-19 16:46:45 +0100

    Update deploy_monitoring.sh

--- k8s_templates/deploy_monitoring.sh
+++ k8s_templates/deploy_monitoring.sh
@@ -2,5 +2,5 @@ kubectl apply -f htcondor-exporter.yaml
 kubectl create namespace monitoring
 kubectl create configmap prometheus-example-cm --from-file configs/prometheus.yml -n monitoring
 kubectl apply -f prometheus.yaml
-kubectl create configmap prometheus-adapter-example-cm --from-file configs/prometheus_adapter.yml
+kubectl create configmap prometheus-adapter-example-cm --from-file configs/prometheus-adapter.yml
 kubectl apply -f prometheus-adapter.yaml
