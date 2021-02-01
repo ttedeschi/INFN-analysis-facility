82d3952cb50a7ecb83fc401c6f036ce360f7f853
Tommaso Tedeschi <tedesk95@gmail.com>
2021-02-01 17:34:22 +0100

    customize Jupyterhub

--- docker-images/jupyterhub/hub_config.py
+++ docker-images/jupyterhub/hub_config.py
@@ -82,7 +82,8 @@ if 'JUPYTERHUB_CRYPT_KEY' not in os.environ:
 
 c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.KubeSpawner.cmd = ['jupyterhub-singleuser', '--allow-root']
-c.KubeSpawner.image = 'dodasts/spark:v3.0.1'
+c.KubeSpawner.image = 'ttedesch/spark:root6.22-davix-permission'
+#c.KubeSpawner.image = 'dodasts/spark:v3.0.1'
 
 # TODO: PUT ENV
 
