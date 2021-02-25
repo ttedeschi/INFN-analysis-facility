172e2d957afd691666565dd8cd7ed187bdb05d6f
root <root@vnode-1.localdomain>
2021-02-25 17:05:19 +0000

    update

--- scripts/deploy_jupyterhub.sh
+++ scripts/deploy_jupyterhub.sh
@@ -1,4 +1,4 @@
 export KUBECONFIG=/etc/kubernetes/admin.conf
 helm repo add infnAF https://ttedeschi.github.io/INFN-analysis-facility/helm-charts
 helm repo update
-helm install hub infnAF/jupyterhub --set hub.host=$1 --set hub.image=ttedesch/jupyterhub:1.0.0 --set hub.nodeLabel=k8s --kubeconfig /etc/kubernetes/admin.conf
+helm install hub infnAF/jupyterhub --set hub.host=$1 --set hub.image=ttedesch/jupyterhub:1.0.0 --kubeconfig /etc/kubernetes/admin.conf
